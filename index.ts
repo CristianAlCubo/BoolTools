eb36cc685ba711849fdcc2f2b8423c58a902add8
Cristian Cubillos <ccubillosreyes123@gmail.com>
2023-04-05 13:46:49 -0500

    Changed package.json

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
+export * from './src/BoolTools/BooleanTree'
 export * from './src/BoolTools/BooleanChart'
-export * from './src/BoolTools/BooleanChart'
-export * from './src/BoolTools/BooleanChart'
+export * from './src/BoolTools/BoolStatementEngine'
